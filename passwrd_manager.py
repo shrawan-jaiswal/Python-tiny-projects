38d8d7c93f2a0893eb325494878b8809db868e89
shrawan-jaiswal <shrawanjaiyswal10@gmail.com>
2024-07-13 11:13:42 +0545

    all

--- passwrd_manager.py
+++ passwrd_manager.py
@@ -1,15 +1,34 @@
-master_pwd = input("What is your Master Password?: ")
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_filee:
+        key_filee.write(key)'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close
+    return key
+
+
+
+key = load_key()
+fer = Fernet(key)
 
 def view():
     with open("password.txt", "r") as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, "| Password: ", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     with open("password.txt", "a") as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
